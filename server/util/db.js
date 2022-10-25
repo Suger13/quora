20ae61c33ee868e38131706c6713c01ccbbfd3fc
Suger13 <muteki.scarlette@gmail.com>
2022-10-25 14:23:51 +0700

    Revert "create server"
    This reverts commit 7b45ecc1a0acedc62f8adc66299ad422daafc614.

--- server/util/db.js
+++
@@ -1,9 +0,0 @@
-import * as pg from 'pg'
-import dotenv from 'dotenv'
-dotenv.config()
-
-const { Pool } = pg.defaults
-
-export const pool = new Pool({
-    connectionString : `postgresql://postgres:${process.env.PGPASSWORD}@localhost:5432/quora`
-})
